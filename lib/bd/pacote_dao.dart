983a3afec36c106d5c36a0113d2a2341ff0b4875
MirellyMaria1 <mmmp1@aluno.ifal.edu.br>
2024-12-09 10:45:01 -0300

    Alterei o pacotedao

--- lib/bd/pacote_dao.dart
+++ lib/bd/pacote_dao.dart
@@ -3,18 +3,18 @@ import 'package:untitled3/domain/pacote.dart';
 import 'package:sqflite/sqflite.dart';
 
 class PacoteDao {
-  // listarPacotes() async {
-  //   Database database = await DBHelper().initDB();
-  //
-  //   String sql = 'SELECT * FROM PACOTE;';
-  //   var result = await database.rawQuery(sql);
-  //
-  //   List<PacoteTuristico> lista = [];
-  //   for (var json in result) {
-  //     PacoteTuristico pacote = PacoteTuristico.fromJson(json);
-  //     lista.add(pacote);
-  //   }
-  //
-  //   return lista;
-  // }
+  listarPacotes() async {
+    Database database = await DBHelper().initDB();
+
+    String sql = 'SELECT * FROM PACOTE;';
+    var result = await database.rawQuery(sql);
+
+    List<PacoteTuristico> lista = [];
+    for (var json in result) {
+      PacoteTuristico pacote = PacoteTuristico.fromJson(json);
+      lista.add(pacote);
+    }
+
+    return lista;
+  }
 }
